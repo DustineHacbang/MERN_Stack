ccdb6d10bf562745bf8c86e0da0a9e924cb181a5
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-09-09 12:40:51 -0700

    todo

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React,{useState} from "react";
 import './App.css';
 
 function App() {
+  
+  const [newTodo, setNewTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleNewTodoSubmit= (event) => {
+    event.preventDefault();
+    setTodos([...todos, newTodo])
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign:"center"}}>
+      <form 
+      onSubmit ={(event) =>{
+          handleNewTodoSubmit =(event);
+      }}>
+
+        <input 
+        onChange ={(event) =>{
+            setNewTodo(event.target.value);
+        }} 
+        type ="text"
+        value={newTodo}
+        />
+
+
+        <div>
+          <button>Submit</button>
+        </div>
+      
+      </form>
+
+        {
+          todos.map((todo, i) =>{
+            return (
+            <div key = {i}>
+              <span>{todo}</span>
+            </div>
+            );
+        })}
+
+
     </div>
   );
 }
