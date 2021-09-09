91fc8310db3262a229abb4dc24c94ce01da34f26
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-09-09 10:08:36 -0700

    to do list

--- hook_more_form/src/components/Form.jsx
+++ hook_more_form/src/components/Form.jsx
@@ -7,7 +7,16 @@ const Form = (props) => {
         "lastName": "",
         "email": "",
         "password": "",
+        "confirmPassword": "",
 
+    })
+
+    const[ validState, setValidState] = useState({
+        "firstName": false,
+        "lastName": false,
+        "email": false,
+        "password": false,
+        "confirmPassword": false,
     })
 
     // const [firstName, setFirstName] = useState("")
@@ -22,16 +31,54 @@ const Form = (props) => {
         })
     }
 
-    const submitHandler =(event) =>{ 
+    const submitHandler = (e) => {
+        let firstName = false;
+        let lastName = false;
+        let email = false;
+        let password = false;
+        let confirmPassword = false; 
     // stops the from from making a request
-    event.preventDefault()
+    e.preventDefault();
         setFormState({
             "firstName": "",
             "lastName": "",
             "email": "",
-            "password": "",            
+            "password": "",
+            "confirmPassword": "",                
         })
-    }
+        //first name Must not be empty
+        if(formState.firstName.length == 0){
+            firstName = true;
+        }
+        //last name Must not be empty
+        if(formState.lastName.length > 0){
+            lastName = true;
+        }
+        // Email Verification
+        if(! (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/).test(formState.email)){
+        email = true;
+        }
+
+        // Password Length Verification
+        if(formState.password.length < 5){
+            password = true;
+        }
+        //Password Verification
+        if(formState.password.length !=  formState.confirmPassword){
+            confirmPassword = true;
+        }
+        setValidState({
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword
+        })
+
+
+
+
+
 
     return (
 <fieldset>
@@ -41,18 +88,27 @@ const Form = (props) => {
             <p>
                 First Name:
                 <input onChange={changeHandler} type="text" name="firstName" id = "" value ={formState.firstName}/>
+                { (validState.firstName) ? <p style ={{"color": "red"}}> You must include a first name</p>: null }
             </p>
             <p>
                 Last Name:
                 <input onChange={changeHandler} type="text" Last Name="lastName" id = "" value ={formState.lastName} />
+                { (validState.lastName) ? <p style ={{"color": "red"}}> You must include a last name</p>: null }
             </p>  
             <p>
                 Email:
                 <input onChange={changeHandler} type="text" Last Name="email" id = "" value ={formState.email} />
+                { (validState.email) ? <p style ={{"color": "red"}}> Email must be valid</p>: null }
             </p>
             <p>
                 Password:
                 <input onChange={changeHandler} type="text" Last Name="password" id = "" value ={formState.password} />
+                { (validState.password) ? <p style ={{"color": "red"}}> Password must be 5 characters</p>: null }
+            </p>
+            <p>
+                Confirm Password:
+                <input onChange={changeHandler} type="text" Last Name="confirm_password" id = "" value ={formState.confirmPassword} />
+                { (validState.confirmPassword) ? <p style ={{"color": "red"}}> Password and Confirm Password Must match</p>: null }
             </p>
             <button>Submit</button>
             
@@ -62,4 +118,4 @@ const Form = (props) => {
     )
 }
 
-export default Form
+export defualt Form
