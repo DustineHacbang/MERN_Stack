c0fb7834234732fd9770f433a6df883992c270fc
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-09-09 15:53:10 -0700

    todolist

--- to_do_list/src/App.js
+++ to_do_list/src/App.js
@@ -5,17 +5,51 @@ function App() {
   
   const [newTodo, setNewTodo] = useState("");
   const [todos, setTodos] = useState([]);
+  
 
   const handleNewTodoSubmit= (event) => {
     event.preventDefault();
-    setTodos([...todos, newTodo])
+
+    if(newTodo.length == 0){
+      return;
+    }
+
+    const todoItem = {
+    text: newTodo,
+    complete: false
+    }
+
+    setTodos([...todos, todoItem]);
   };
+
+  const handleTodoDelete = (delIdx) =>{
+    const filteredTodos = todos.filter((todo,i) => {
+      return i != delIdx;
+    });
+    setTodos(filteredTodos)
+
+  };
+
+  const handleToggleComplete = (idx) => {
+    const updatedTodos = todos.map((todo,i) => {
+      if (idx == i){
+        todo.complete = !todo.complete;
+        // const updatedTodos ={... todo, complete: !todo.complete};
+        // return updatedTodos
+      }
+
+
+      return todo;
+    });
+
+    setTodos(updatedTodos);
+  }
 
   return (
     <div style={{textAlign:"center"}}>
       <form 
-      onSubmit ={(event) =>{
-          handleNewTodoSubmit =(event);
+      onSubmit ={(event) => {
+          handleNewTodoSubmit(event);
       }}>
 
         <input 
@@ -34,10 +68,17 @@ function App() {
       </form>
 
         {
-          todos.map((todo, i) =>{
+          todos.map((todo, i) => {
             return (
             <div key = {i}>
-              <span>{todo}</span>
+              <input on onChange={(event) =>{
+                handleToggleComplete(i);
+              }}
+              checked={todo.complete}  type="checkbox" />
+              <span>{todo.text} </span>
+              <button onClick={(event) => {
+                handleTodoDelete(i);
+              }}>DELETE</button>
             </div>
             );
         })}
