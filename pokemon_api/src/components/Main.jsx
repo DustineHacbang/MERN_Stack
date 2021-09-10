ecb13466df91aa376a976fd196eddb56fe4f67a9
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-09-10 14:07:06 -0700

    pokemon

---
+++ pokemon_api/src/components/Main.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from "react";
+import axios from 'axios'
+
+const Main = () => {
+
+    const[pokemon,setState] = useState([])
+
+    // const swAPI =() => {
+    //     fetch("https://pokeapi.co/api/v2/pokemon")
+    //         .then(response => response.json())
+    //         .then(jsonRes => console.log(jsonRes))
+    //         .catch(error => console.log(error))
+    // }
+
+    const axiosCall=(e)=>{
+        axios.get("https://pokeapi.co/api/v2/pokemon?limit=807")
+            .then(response => setState(response.data.results)) 
+            .catch(error => console.log(error))
+    }
+
+
+
+    return(
+        <div>
+            <button onClick={axiosCall}>Get Pokemon</button>
+            {pokemon.map((pokemon,index) => {
+                return (<div key={index}>Name: {pokemon.name}</div>)
+        })}
+        </div>
+    )
+    }
+
+export default Main
