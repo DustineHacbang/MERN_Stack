c1c58178ef6e2f179b117435cec85dcfcc86e32d
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-09-19 10:46:37 -0700

    demo

---
+++ book_demo/server/server.js
@@ -0,0 +1,14 @@
+////1. import dependencies
+const express = require("express")
+const cors = require("cors")
+const app = express()
+const port = 8000
+
+////1.5. Config mongoose
+
+////2. config express
+
+////3.attach routes to your express server
+
+////4.run express server
+app.listen(port,() => console.log(`EXPRESS SERVER RUNING ON ${port}`))
