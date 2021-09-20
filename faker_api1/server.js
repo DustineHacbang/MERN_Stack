134a15c83f4215c25fde7f5818cffef5dc0cfd86
Dustine Hacbang <dustinehacbang526@gmail.com>
2021-09-20 13:56:39 -0700

    Faker api

--- faker_api1/server.js
+++ faker_api1/server.js
@@ -1,17 +1,62 @@
+
+
 //1.IMPORT EXPRESS AND CREATE AN INSTANCE OF EXPRESS SERVER
 const express = require("express")
 const app = express()
+var faker = require("faker")
 const port = 8000
 
 
-//CONFI FOR YOUR EXPRESS SERVER
+
+
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
 
+class User{
+    constructor(){
+        this._id = faker.random.uuid()
+        this.firstName=faker.name.firstName()
+        this.lastName= faker.name.lastName()
+        this.phoneNum=faker.phone.phoneNumber()
+        this.email = faker.internet.email()
+        this.internet = faker.internet.password()
+    }
+}
 
-//DEFINE ROUTES FOR YOUR EXPRESS SERVER
-const UserRoutes = require("./routes/user.routes")
-UserRoutes(app)
+class Company{
+    constructor(){
+        this._id = faker.random.uuid()
+        this.name=faker.company.companyName()
+        this.adress= {
+            street: faker.address.streetName(),
+            street: faker.address.city(),
+            street: faker.address.city(),
+        }
+    }
+}
+
+let user1=  new User()
+console.log(user1)
+
+let company1=  new Company()
+console.log(company1)
+
+app.get("/api/users/new" , (req,res) =>{
+    let user1=  new User()
+    console.log(user1)
+    res.json({"result": user1})
+})
+
+app.get("/api/companies/new" , (req,res) =>{
+    let company1=  new Company()
+    console.log(company1)
+    res.json({"result": company1})
+})
+
+app.get("/api/users/company" , (req,res) =>{
+    let user1=  new User()
+    let company1=  new Company()
+    res.json({"user": user1, "comapny": company1})
+})
 
 //RUN YOUR EXPRESS SERVER
 app.listen(port,() => console.log("EXPRESS SERVER RUNNING ON PORT:" + port ))
